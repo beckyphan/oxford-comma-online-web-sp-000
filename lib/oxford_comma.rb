6d9a124ff5687142f1472c8fa9f48aa7a43cd66a
Becky Phan <beckyphan@live.com>
2019-05-24 03:15:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,7 @@
 def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
-  elsif array.length > 2 
-    array.join
+  elsif array.length >= 3 
+    last_item = array[-1]
+    array[-1] = last_item.concatenate("and ")
 end
